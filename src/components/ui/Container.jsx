e215a1450327b4437a4c8c88d072fc801e6a0778
flowagenci <flowagencyteam@gmail.com>
2026-03-07 20:04:58 +0530

    Deploy compiled site to main

--- src/components/ui/Container.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function Container({ children, className = '' }) {
-    return (
-        <div className={`max-w-7xl mx-auto px-6 sm:px-8 ${className}`}>
-            {children}
-        </div>
-    );
-}
